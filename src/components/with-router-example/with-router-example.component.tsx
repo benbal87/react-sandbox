f08f6e6d8a48c1e0785969992e57b1ec0c6d8a10
Benedek Balázs <benbal87@gmail.com>
2022-06-14 07:27:44 +0700

    with router example appended

--- src/components/with-router-example/with-router-example.component.tsx
+++ src/components/with-router-example/with-router-example.component.tsx
@@ -1,12 +1,45 @@
 import React from 'react'
-import { RouteComponentProps, withRouter } from 'react-router-dom'
+import {
+  RouteComponentProps,
+  useHistory,
+  useLocation,
+  useParams,
+  withRouter
+} from 'react-router-dom'
+import { createStructuredSelector } from 'reselect'
+import { selectCounterValue } from 'redux/counter/counter.selector'
+import { connect, ConnectedProps } from 'react-redux'
 
-function WithRouterExample({ match, location, history }: RouteComponentProps) {
+export interface WithRouterExampleProps
+  extends WithRouterExamplePropsFromRedux,
+    RouteComponentProps {}
+
+function WithRouterExample({
+  match,
+  location,
+  history,
+  counterValue
+}: WithRouterExampleProps) {
+  console.log('counterValue', counterValue)
   console.log('match', match)
   console.log('location', location)
   console.log('history', history)
+  const historyFromUseHistory = useHistory()
+  console.log('history from use history', historyFromUseHistory)
+  const locationFromUseLocation = useLocation()
+  console.log('location from use location', locationFromUseLocation)
+  const paramsFromUseParams = useParams()
+  console.log('params from use params', paramsFromUseParams)
 
   return <div>RouterTryout</div>
 }
 
-export default withRouter(WithRouterExample)
+const mapStateToProps = createStructuredSelector({
+  counterValue: selectCounterValue
+})
+
+const connector = connect(mapStateToProps)
+
+export type WithRouterExamplePropsFromRedux = ConnectedProps<typeof connector>
+
+export default withRouter(connector(WithRouterExample))
